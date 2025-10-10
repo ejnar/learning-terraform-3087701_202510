f171940ea429cf313723aaa716f6e935e2b4b19b
Ejnar Aakerman <eaakerman@gmail.com>
2025-10-10 15:31:07 +0200

    update with correct syntax

--- main.tf
+++ main.tf
@@ -19,8 +19,8 @@ resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
 
-  subnet_id     = subnet-12a8ab3f  # Explicitly specify subnet
-  vpc_security_group_ids = sg-feb60481 # use IDs
+  subnet_id     = aws_subnet.subnet-12a8ab3f.id  # Explicitly specify subnet
+  vpc_security_group_ids = [aws_security_group.sg-feb60481.id] # use IDs
 
   tags = {
     Name = "HelloWorld"

6c84c1bddc40f66e1ef8f401e0f6c977a24c52dd
Ejnar Aakerman <eaakerman@gmail.com>
2025-10-21 17:13:48 +0200

    Add HTTPS

--- main.tf
+++ main.tf
@@ -82,18 +82,26 @@ resource "aws_security_group" "allow_ssh_http" {
   description = "Allow SSH and HTTP traffic"
   vpc_id      = aws_vpc.main.id
 
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  #ingress {
+  #  description = "SSH"
+  #  from_port   = 22
+  #  to_port     = 22
+  #  protocol    = "tcp"
+  #  cidr_blocks = ["0.0.0.0/0"]
+  #}
 
   ingress {
     description = "HTTP"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

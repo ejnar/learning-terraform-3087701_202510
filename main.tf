30acf3384f7283fba4a501f99bc624e8a577d2eb
Ejnar Aakerman <eaakerman@gmail.com>
2025-10-22 14:09:20 +0200

    comment old sg

--- main.tf
+++ main.tf
@@ -77,46 +77,46 @@ resource "aws_route_table_association" "public_assoc" {
 # ------------------------------
 # 6. Create Security Group
 # ------------------------------
-resource "aws_security_group" "allow_ssh_http" {
-  name        = "allow-ssh-http"
-  description = "Allow SSH and HTTP traffic"
-  vpc_id      = aws_vpc.main.id
+#resource "aws_security_group" "allow_ssh_http" {
+#  name        = "allow-ssh-http"
+#  description = "Allow SSH and HTTP traffic"
+#  vpc_id      = aws_vpc.main.id
 
-  #ingress {
-  #  description = "SSH"
-  #  from_port   = 22
-  #  to_port     = 22
-  #  protocol    = "tcp"
-  #  cidr_blocks = ["0.0.0.0/0"]
-  #}
+#  ingress {
+#    description = "SSH"
+#    from_port   = 22
+#    to_port     = 22
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    description = "HTTP"
+#    from_port   = 80
+#    to_port     = 80
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  ingress {
+#    description = "HTTPS"
+#    from_port   = 443
+#    to_port     = 443
+#    protocol    = "tcp"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#  egress {
+#    from_port   = 0
+#    to_port     = 0
+#    protocol    = "-1"
+#    cidr_blocks = ["0.0.0.0/0"]
+#  }
 
-  tags = {
-    Name = "allow-ssh-http"
-  }
-}
+#  tags = {
+#    Name = "allow-ssh-http"
+#  }
+#}
 
 
 resource "aws_instance" "web" {

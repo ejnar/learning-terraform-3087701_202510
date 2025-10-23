5bb58f121d803db863b08358ac7067dd16e61862
Ejnar Aakerman <eaakerman@gmail.com>
2025-10-23 13:26:07 +0200

    fix listeners

--- main.tf
+++ main.tf
@@ -54,13 +54,13 @@ module "alb" {
     }
   ]
 
-  http_tcp_listeners = [
-    {
+  listeners = {
+    ex-http = {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
     }
-  ]
+  }
 
   tags = {
     Environment = "Dev"

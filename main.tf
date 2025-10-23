96b11318b3237ddbed26f5bb59269bfd2b4526f6
Ejnar Aakerman <eaakerman@gmail.com>
2025-10-23 16:09:02 +0200

    add azs

--- main.tf
+++ main.tf
@@ -98,9 +98,9 @@ module "web_vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  azs             = ["us-west-2a"]
-  public_subnets  = ["10.0.1.0/24"]
-  private_subnets = ["10.0.3.0/24"]
+  azs             = ["us-west-2a", "us-west-2b"]
+  public_subnets  = ["10.0.1.0/24","10.0.2.0/24"]
+  private_subnets = ["10.0.5.0/24","10.0.6.0/24"]
 
   enable_nat_gateway = false
   enable_vpn_gateway = false

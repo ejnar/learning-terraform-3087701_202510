4d442902a8acc6c1afa10f88b7cc5f3aeababce6
Ejnar Aakerman <eaakerman@gmail.com>
2025-10-24 12:52:43 +0200

    update alb

--- main.tf
+++ main.tf
@@ -104,9 +104,12 @@ listeners = {
     ex-http = {
       port     = 80
       protocol = "HTTP"
-      default_action_type = "forward"
+      forward = {
+        target_group_key = "ex-instance"
+      }
+      #default_action_type = "forward"
       #target_group_index  = 0
-      target_group_arn    = aws_lb_target_group.web.arn
+      #target_group_arn    = aws_lb_target_group.web.arn
     }
   }
 

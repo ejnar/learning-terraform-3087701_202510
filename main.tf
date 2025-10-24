cf1f9526af4fc48c0293184ae88b3fdf2bc8b69c
Ejnar Aakerman <eaakerman@gmail.com>
2025-10-24 18:01:42 +0200

    fix3

--- main.tf
+++ main.tf
@@ -39,24 +39,25 @@ module "web_vpc" {
 
 module "web_asg" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "~> 9.0"
+  version = "~> 7.0"
   name = "web"
   
   min_size = 1
   max_size = 2
 
   vpc_zone_identifier = module.web_vpc.public_subnets
-  #target_group_arns   = [aws_lb_target_group.web.arn]
   security_groups     = [module.web_sg.security_group_id]
 
   instance_type       = var.instance_type
   image_id            = data.aws_ami.app_ami.id
   
-  load_balancers = [
-    {
-      target_group_arn = aws_lb_target_group.web.arn
-    }
-  ]
+  #load_balancers = [
+  #  {
+  #    target_group_arn = aws_lb_target_group.web.arn
+  #  }
+  #]
+
+  target_group_arns   = [aws_lb_target_group.web.arn]
 
   tags = {
     Environment = "dev"

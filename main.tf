bab0afcd7459c488392990fe9c0fb5960dd5dd75
Ejnar Aakerman <eaakerman@gmail.com>
2025-11-04 15:31:21 +0100

    update asg

--- main.tf
+++ main.tf
@@ -113,45 +113,160 @@ data "aws_ami" "amazon_linux" {
 
 module "asg" {
   source  = "terraform-aws-modules/autoscaling/aws"
-  version = "9.0.0"
 
-  name                = "demo-asg"
-  min_size            = 1
-  max_size            = 2
-  desired_capacity    = 1
-  health_check_type   = "EC2"
-  vpc_zone_identifier = module.vpc.public_subnets
+  # Autoscaling group
+  name = "example-asg"
 
-  # Launch template defaults (replaces launch_template)
-  launch_template_defaults = {
-    image_id      = data.aws_ami.amazon_linux.id
-    instance_type = var.instance_type
-    key_name      = var.key_name
+  min_size                  = 0
+  max_size                  = 1
+  desired_capacity          = 1
+  wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  # vpc_zone_identifier       = ["subnet-1235678", "subnet-87654321"]
+  vpc_zone_identifier       = module.vpc.public_subnets
 
-    user_data = base64encode(<<-EOF
-                #!/bin/bash
-                yum install -y httpd
-                echo "Hello from $(hostname)" > /var/www/html/index.html
-                systemctl enable httpd
-                systemctl start httpd
-                EOF
-    )
-
-    network_interfaces = [
-      {
-        security_groups = [module.ec2_sg.security_group_id]
-      }
-    ]
-  }
-
-  # Attach ALB target groups
-  target_groups = [module.alb.target_groups["web"].arn]
-
-  tags = [
+  initial_lifecycle_hooks = [
     {
-      key                 = "Name"
-      value               = "demo-instance"
-      propagate_at_launch = true
+      name                  = "ExampleStartupLifeCycleHook"
+      default_result        = "CONTINUE"
+      heartbeat_timeout     = 60
+      lifecycle_transition  = "autoscaling:EC2_INSTANCE_LAUNCHING"
+      notification_metadata = jsonencode({ "hello" = "world" })
+    },
+    {
+      name                  = "ExampleTerminationLifeCycleHook"
+      default_result        = "CONTINUE"
+      heartbeat_timeout     = 180
+      lifecycle_transition  = "autoscaling:EC2_INSTANCE_TERMINATING"
+      notification_metadata = jsonencode({ "goodbye" = "world" })
     }
   ]
+
+  instance_refresh = {
+    strategy = "Rolling"
+    preferences = {
+      checkpoint_delay       = 600
+      checkpoint_percentages = [35, 70, 100]
+      instance_warmup        = 300
+      min_healthy_percentage = 50
+      max_healthy_percentage = 100
+    }
+    triggers = ["tag"]
+  }
+
+  # Launch template
+  launch_template_name        = "example-asg"
+  launch_template_description = "Launch template example"
+  update_default_version      = true
+
+  #image_id          = "ami-ebd02392"
+  image_id          = data.aws_ami.amazon_linux.id
+  #instance_type     = "t3.micro"
+  instance_type     = var.instance_type
+  
+  ebs_optimized     = true
+  enable_monitoring = true
+
+  # IAM role & instance profile
+  create_iam_instance_profile = true
+  iam_role_name               = "example-asg"
+  iam_role_path               = "/ec2/"
+  iam_role_description        = "IAM role example"
+  iam_role_tags = {
+    CustomIamRole = "Yes"
+  }
+  iam_role_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  block_device_mappings = [
+    {
+      # Root volume
+      device_name = "/dev/xvda"
+      no_device   = 0
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 20
+        volume_type           = "gp2"
+      }
+    }, {
+      device_name = "/dev/sda1"
+      no_device   = 1
+      ebs = {
+        delete_on_termination = true
+        encrypted             = true
+        volume_size           = 30
+        volume_type           = "gp2"
+      }
+    }
+  ]
+
+  capacity_reservation_specification = {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options = {
+    core_count       = 1
+    threads_per_core = 1
+  }
+
+  credit_specification = {
+    cpu_credits = "standard"
+  }
+
+  instance_market_options = {
+    market_type = "spot"
+    spot_options = {
+      block_duration_minutes = 60
+    }
+  }
+
+  # This will ensure imdsv2 is enabled, required, and a single hop which is aws security
+  # best practices
+  # See https://docs.aws.amazon.com/securityhub/latest/userguide/autoscaling-controls.html#autoscaling-4
+  metadata_options = {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  network_interfaces = [
+    {
+      delete_on_termination = true
+      description           = "eth0"
+      device_index          = 0
+      security_groups       = [module.ec2_sg.security_group_id]
+    },
+    {
+      delete_on_termination = true
+      description           = "eth1"
+      device_index          = 1
+      security_groups       = [module.ec2_sg.security_group_id]
+    }
+  ]
+
+  placement = {
+    availability_zone = "us-west-1b"
+  }
+
+  tag_specifications = [
+    {
+      resource_type = "instance"
+      tags          = { WhatAmI = "Instance" }
+    },
+    {
+      resource_type = "volume"
+      tags          = { WhatAmI = "Volume" }
+    },
+    {
+      resource_type = "spot-instances-request"
+      tags          = { WhatAmI = "SpotInstanceRequest" }
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+    Project     = "megasecret"
+  }
 }

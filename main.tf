bc1bdf534540278834c7f11b3d7e9c5def2cbc3b
Ejnar Aakerman <eaakerman@gmail.com>
2025-11-04 16:19:48 +0100

    remove asg

--- main.tf
+++ main.tf
@@ -29,27 +29,6 @@ module "blog_vpc" {
   }
 }
 
-
-
-module "blog_asg" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "6.5.2"
-
-  name = "blog"
-
-  min_size            = 1
-  max_size            = 2
-  desired_capacity    = 1
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-
-  target_group_arns   = [module.blog_alb.target_group_arns]
-
-  security_groups     = [module.blog_sg.security_group_id]
-  
-  instance_type       = var.instance_type
-  image_id            = data.aws_ami.app_ami.id
-}
-
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"

bc7c8e9f2d8d41895c3497e15291e16f2c7c22fe
Ejnar Aakerman <eaakerman@gmail.com>
2025-11-04 16:18:56 +0100

    later update

--- main.tf
+++ main.tf
@@ -1,115 +1,97 @@
-################################################
-# VPC
-################################################
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.1.2"
-
-  name = "demo-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs            = ["us-east-1a", "us-east-1b"]
-  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  enable_nat_gateway = false
-
-  tags = {
-    Environment = "demo"
-  }
-}
-
-################################################
-# Security group for ALB
-################################################
-module "alb_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-
-  name        = "alb-sg"
-  description = "Allow HTTP access"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-  ingress_rules       = ["http-80-tcp"]
-  egress_rules        = ["all-all"]
-}
-
-################################################
-# ALB with listener and target group
-################################################
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "9.0.0"
-
-  name               = "demo-alb"
-  load_balancer_type = "application"
-  vpc_id             = module.vpc.vpc_id
-  subnets            = module.vpc.public_subnets
-  security_groups    = [module.alb_sg.security_group_id]
-
-  # Target groups map
-  target_groups = {
-    web = {
-      name_prefix      = "tgweb"          # <=6 chars
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      health_check = {
-        path     = "/"
-        protocol = "HTTP"
-      }
-      create_attachment = false           # ASG attaches later
-    }
-  }
-
-  # Listeners map
-  listeners = {
-    http = {
-      port     = 80
-      protocol = "HTTP"
-      default_action = {
-        type             = "forward"
-        target_group_key = "web"
-      }
-    }
-  }
-
-  tags = {
-    Environment = "demo"
-  }
-}
-
-################################################
-# Security group for EC2
-################################################
-module "ec2_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-
-  name        = "ec2-sg"
-  description = "Allow traffic from ALB"
-  vpc_id      = module.vpc.vpc_id
-
-  ingress_with_source_security_group_id = [
-    {
-      from_port                = 80
-      to_port                  = 80
-      protocol                 = "tcp"
-      source_security_group_id = module.alb_sg.security_group_id
-    }
-  ]
-
-  egress_rules = ["all-all"]
-}
-
-################################################
-# Auto Scaling Group
-################################################
-data "aws_ami" "amazon_linux" {
+data "aws_ami" "app_ami" {
   most_recent = true
-  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
+
+module "blog_asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.2"
+
+  name = "blog"
+
+  min_size            = 1
+  max_size            = 2
+  desired_capacity    = 1
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+
+  target_group_arns   = [module.blog_alb.target_group_arns]
+
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
+}
+
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = module.blog_vpc.vpc_id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,1 @@
-output "alb_dns_name" {
-  value = module.alb.dns_name
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
-
 
